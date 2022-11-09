e9bcaf99470a2e9373912d775bbd9683e103fc01
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-11-09 14:01:00 +0100

    ADD: POE model

---
+++ src/models/poe.ts
@@ -0,0 +1,25 @@
+export class POE {
+    public title: string = ''
+    private beginDate!: Date
+    private endDate!: Date
+
+    public setBeginDate(date: Date): void {
+        if (this.endDate !== undefined) {
+            if (date < this.endDate) {
+                this.beginDate = date
+            }
+        } else {
+            this.beginDate = date;
+        }
+    }
+
+    public setEndDate(date: Date): void {
+        if (this.beginDate !== undefined) {
+            if (date > this.beginDate) {
+                this.endDate = date
+            }
+        } else {
+            this.endDate = date
+        }
+    }
+}
