2bfb94d19954604090331979cea31a5a985058ae
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-11-09 11:03:02 +0100

    ADD: main.ts UPDATE: package.json to automate app run

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/**
+ * main : Entry point of application
+ * @version 1.0.0
+ * @date 2022-11-09
+ * @author Aélion <jla.webprojet@gmail.com>
+ */
+class Main {
+    public constructor() {
+        console.log('Hello TypeScript')
+    }
+}
+
+// Instanciate the Main class (constructor will be invoked)
+new Main()
